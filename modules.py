a4568b6a8f210e0bf652ec43b89dc2b99bfda1e1
Mauricio Neut <m.neut.santos@gmail.com>
2022-08-15 03:30:38 -0400

    Creacion del archivo modules.py con metodo que retorna listas de subdirectorios y archivos en direccion indicada

---
+++ modules.py
@@ -0,0 +1,13 @@
+import os
+
+def dir_list(dir_path):
+    path_info = list(os.walk(dir_path))[0]
+    files = [f for f in path_info[2] if ((not f.startswith('.')) and (not f.endswith('.ini')))]
+    dirs = [d for d in path_info[1] if not d.startswith('.')]
+    root_dir = path_info[0]
+
+    return [root_dir, dirs, files]
+
+print(dir_list("C:\\Users\\Mauricio\\Desktop\\Prueba\\A"))
+
+#print(list(os.walk("C:\\Users")))
